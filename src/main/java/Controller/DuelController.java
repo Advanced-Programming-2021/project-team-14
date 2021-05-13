d4ababe52276bcef4dbd9af759c8d70d1b3c97b8
Soltaniant <s.soltaniant@gmail.com>
2021-05-13 18:37:28 +0430

    duel controller bugs removed (tested till new game starting)

--- src/main/java/Controller/DuelController.java
+++ src/main/java/Controller/DuelController.java
@@ -16,15 +16,15 @@ public class DuelController {
     }
 
     private static String startGame(JSONObject request) {
-        if (!User.doesUsernameExist(request.getString("second-player")))
+        if (!User.doesUsernameExist(request.getString(Strings.SECOND_PLAYER.getLabel())))
             return Strings.PLAYER_NOT_EXIST.getLabel();
-        User first = User.getUserByName(request.getString("token"));
-        User second = User.getUserByName(request.getString("second-player"));
+        User first = User.getUserByName(request.getString(Strings.TOKEN.getLabel()));
+        User second = User.getUserByName(request.getString(Strings.SECOND_PLAYER.getLabel()));
         if (!first.doesHaveActiveDeck()) return String.format(Strings.NO_ACTIVE_DECK.getLabel(), first.getUsername());
         if (!second.doesHaveActiveDeck()) return String.format(Strings.NO_ACTIVE_DECK.getLabel(), second.getUsername());
         if (!first.getDeck(first.getActiveDeck()).isValid()) return String.format(Strings.INVALID_DECK.getLabel(), first.getUsername());
         if (!second.getDeck(second.getActiveDeck()).isValid()) return String.format(Strings.INVALID_DECK.getLabel(), first.getUsername());
-        int rounds = request.getInt("round");
+        int rounds = request.getInt(Strings.ROUNDS_NUMBER.getLabel());
         if (rounds != 1 && rounds != 3)
             return Strings.NUMBER_OF_ROUNDS_NOT_SUPPORTED.getLabel();
         game = new Game(first, second, rounds);
