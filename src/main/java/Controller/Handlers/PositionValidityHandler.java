198d1140cae72d1ccae9aa26340ff8b203df1fc9
Soltaniant <s.soltaniant@gmail.com>
2021-05-14 11:40:43 +0430

    simplify and match with the new receiving data from View

--- src/main/java/Controller/Handlers/PositionValidityHandler.java
+++ src/main/java/Controller/Handlers/PositionValidityHandler.java
@@ -7,16 +7,18 @@ import view.enums.CommandTags;
 
 public class PositionValidityHandler extends GameHandler{
     public String handle (JSONObject request, Game game){
-        String command = request.getString(Strings.COMMAND.getLabel());
-        if (command.equals(CommandTags.SELECT_CARD_MONSTER.getLabel()) &&
-            isPositionValid(request.getInt(Strings.MONSTER_POSITION.getLabel()), 5)){
-                return Strings.INVALID_SELECTION.getLabel();
-        } else if (command.equals(CommandTags.SELECT_CARD_SPELL.getLabel()) &&
-                   isPositionValid(request.getInt(Strings.SPELL_POSITION.getLabel()), 5)){
-            return Strings.INVALID_SELECTION.getLabel();
-        } else if (command.equals(CommandTags.SELECT_HAND.getLabel()) &&
-                   isPositionValid(request.getInt(Strings.HAND_POSITION.getLabel()), game.getBoard().getMainPlayer().getHand().getSize())){
-            return Strings.INVALID_SELECTION.getLabel();
+
+        String area = request.getString(Strings.AREA.getLabel());
+        int position = request.getInt(Strings.POSITION.getLabel());
+
+        switch (area){
+            case "monster":
+            case "spell":
+                if (isPositionValid(position, 5))
+                    return Strings.INVALID_SELECTION.getLabel();
+            case "hand":
+                if (isPositionValid(position, game.getBoard().getMainPlayer().getHand().getSize()))
+                    return Strings.INVALID_SELECTION.getLabel();
         }
 
         return super.handle(request, game);
