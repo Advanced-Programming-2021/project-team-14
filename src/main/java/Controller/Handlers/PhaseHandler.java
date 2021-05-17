121c15a8a79e972db1b798e636390514132c60c2
FatemehAlimoradi <f.alimoradi80@gmail.com>
2021-05-17 16:41:50 +0430

    debug direct attack

--- src/main/java/Controller/Handlers/PhaseHandler.java
+++ src/main/java/Controller/Handlers/PhaseHandler.java
@@ -10,21 +10,23 @@ import view.enums.CommandTags;
 
 import java.util.Objects;
 
-public class PhaseHandler extends GameHandler{
-    public String handle(JSONObject request, Game game){
+public class PhaseHandler extends GameHandler {
+    public String handle(JSONObject request, Game game) {
         Logger.log("phase handler", "checking ...");
 
         Phase expectedPhase = Phase.MAIN_PHASE;
         String command = request.getString("command");
         response = Strings.CANNOT_TAKE_ACTION_IN_THIS_PHASE.getLabel();
-        switch (Objects.requireNonNull(CommandTags.fromValue(command))){
+        switch (Objects.requireNonNull(CommandTags.fromValue(command))) {
 
             case SUMMON:
                 response = Strings.ACTION_NOT_ALlOWED_IN_THIS_PHASE.getLabel();
                 break;
             case ATTACK:
+            case DIRECT_ATTACK:
                 expectedPhase = Phase.BATTLE_PHASE;
                 break;
+
             case ACTIVATE_EFFECT:
                 response = Strings.CANNOT_ACTIVATE_IN_THIS_TURN.getLabel();
                 break;
