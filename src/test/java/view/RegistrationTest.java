29ef097f87ee9e4032ff77d1e767994dbb01d384
Soltaniant <s.soltaniant@gmail.com>
2021-06-23 12:16:09 +0430

    fix some bugs in RegistrationTest

--- src/test/java/view/RegistrationTest.java
+++ src/test/java/view/RegistrationTest.java
@@ -1,38 +1,39 @@
-package Test;
+package view;
 
 import Controller.enums.Responses;
 import model.User;
 import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import view.Request;
 import view.enums.CommandTags;
 import view.enums.Menus;
 
 public class RegistrationTest {
 
-    public JSONObject sendRequest(String menu, CommandTags commandTag, String username, String password, String nickname) {
-        Request.addData("view", menu);
+    public String sendRequest(CommandTags commandTag, String username, String password, String nickname) {
+        Request.addData("view", Menus.REGISTER_MENU.getLabel());
         Request.setCommandTag(commandTag);
         Request.addData("username", username);
         Request.addData("password", password);
         Request.addData("nickname", nickname);
         Request.send();
-        return new JSONObject(Request.getResponse());
+        return Request.getMessage();
     }
 
-    @Test
-    public void createUserSuccessfully() {
-        JSONObject response = sendRequest(Menus.REGISTER_MENU.getLabel(), CommandTags.REGISTER,
-                "username", "1234", "nickname");
-        Assertions.assertEquals(Responses.REGISTER_SUCCESSFUL.getLabel(), response.getString("message"));
-    }
 
     @BeforeEach
     public void createUserForTests() {
-        sendRequest(Menus.REGISTER_MENU.getLabel(), CommandTags.REGISTER,
+        sendRequest(CommandTags.REGISTER,
                 "username", "Password1234", "nickname");
+    }
+    @Test
+    public void createUserSuccessfully() {
+        String response = sendRequest(CommandTags.REGISTER,
+                "usernameCreate", "1234", "nicknameCreate");
+        Assertions.assertEquals(Responses.REGISTER_SUCCESSFUL.getLabel(), response);
     }
 
     @Test
@@ -52,37 +53,37 @@ public class RegistrationTest {
 
     @Test
     public void createUserForSecondTime() {
-        JSONObject response = sendRequest(Menus.REGISTER_MENU.getLabel(), CommandTags.REGISTER,
+        String response = sendRequest(CommandTags.REGISTER,
                 "username", "Password1234", "nickname");
-        Assertions.assertEquals(Responses.USERNAME_ALREADY_EXIST.getLabel().replace("%s", "username"), response.getString("message"));
+        Assertions.assertEquals(Responses.USERNAME_ALREADY_EXIST.getLabel().replace("%s", "username"), response);
     }
 
     @Test
     public void createUserWithSameNickname() {
-        JSONObject response = sendRequest(Menus.REGISTER_MENU.getLabel(), CommandTags.REGISTER,
+        String response = sendRequest(CommandTags.REGISTER,
                 "username2", "Password1234", "nickname");
-        Assertions.assertEquals(Responses.NICKNAME_ALREADY_EXIST.getLabel().replace("%s", "nickname"), response.getString("message"));
+        Assertions.assertEquals(Responses.NICKNAME_ALREADY_EXIST.getLabel().replace("%s", "nickname"), response);
     }
 
     @Test
     public void loginSuccessful() {
-        JSONObject response = sendRequest(Menus.REGISTER_MENU.getLabel(), CommandTags.LOGIN,
+        String response = sendRequest(CommandTags.LOGIN,
                 "username", "Password1234", "nickname");
-        Assertions.assertEquals(Responses.LOGIN_SUCCESSFUL.getLabel(), response.getString("message"));
+        Assertions.assertEquals(Responses.LOGIN_SUCCESSFUL.getLabel(), response);
     }
 
     @Test
     public void loginWithWrongPassword() {
-        JSONObject response = sendRequest(Menus.REGISTER_MENU.getLabel(), CommandTags.LOGIN,
+        String response = sendRequest(CommandTags.LOGIN,
                 "username", "Password123456", "nickname");
-        Assertions.assertEquals(Responses.WRONG_PASSWORD.getLabel(), response.getString("message"));
+        Assertions.assertEquals(Responses.WRONG_PASSWORD.getLabel(), response);
     }
 
     @Test
     public void loginWithWrongUsername() {
-        JSONObject response = sendRequest(Menus.REGISTER_MENU.getLabel(), CommandTags.LOGIN,
+        String response = sendRequest(CommandTags.LOGIN,
                 "username2", "Password1234", "nickname");
-        Assertions.assertEquals(Responses.WRONG_PASSWORD.getLabel(), response.getString("message"));
+        Assertions.assertEquals(Responses.WRONG_PASSWORD.getLabel(), response);
     }
 
 
