bc7fce87daa96029e82e6803afad571bad1ed288
Soltaniant <s.soltaniant@gmail.com>
2021-06-24 14:48:09 +0430

    add deck menu

--- src/main/java/graphic/DeckMenu.java
+++ src/main/java/graphic/DeckMenu.java
@@ -1,4 +1,34 @@
 package graphic;
 
-public class DeckMenu {
+import com.jfoenix.controls.JFXListView;
+import graphic.component.DeckListItem;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Background;
+import model.Deck;
+import model.Strings;
+import view.Request;
+import view.enums.CommandTags;
+import view.enums.Menus;
+
+public class DeckMenu extends Menu{
+    @FXML
+    public JFXListView listView;
+    @FXML
+    public void initialize(){
+        listView.setBackground(Background.EMPTY);
+        currentUser.getDecks().values().forEach(this::addItem);
+        listView.setVerticalGap(10.0);
+    }
+
+    private void addItem(Deck deck) {
+        DeckListItem deckListItem = new DeckListItem(deck);
+        deckListItem.getDelete().setOnMouseClicked(e -> {
+            listView.getItems().remove(deckListItem);
+            Request.addData(Strings.DECK.getLabel(), deck.getName());
+            Request.setCommandTag(CommandTags.DELETE_DECK);
+            setView(Menus.DECK_MENU);
+            Request.send();
+        });
+        listView.getItems().add(deckListItem);
+    }
 }
