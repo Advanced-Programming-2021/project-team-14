6c7c6ebd1a989d09714b73f5eae864528a191ba3
AmirSaeed Ahmadi <amirsaeedahmadi80@yahoo.com>
2021-07-05 20:22:46 +0430

    update Game Play fxml and GamePlay

--- src/main/java/graphic/GamePlay.java
+++ src/main/java/graphic/GamePlay.java
@@ -1,33 +1,73 @@
 package graphic;
 
-import com.jfoenix.controls.JFXProgressBar;
-import graphic.component.*;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
-import javafx.event.EventHandler;
+import graphic.component.Hand;
+import graphic.component.Phases;
+import graphic.component.RivalHand;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.ProgressIndicator;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.MouseDragEvent;
-import javafx.scene.input.TransferMode;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.util.Duration;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import model.game.Duel;
 import model.game.Game;
+import sample.MainGraphic;
 
 
-public class GamePlay extends Menu{
-    public AnchorPane view;
+public class GamePlay extends Menu {
+
+
+    @FXML
+    private AnchorPane view;
+    @FXML
+    private VBox sideCard;
+    @FXML
+    private Circle upperPlayerPhoto;
+    @FXML
+    private Text upperPlayerNickname;
+    @FXML
+    private Text upperPlayerLifePoint;
+    @FXML
+    private ImageView selectedCardImage;
+    @FXML
+    private Text attack;
+    @FXML
+    private Text defense;
+    @FXML
+    private Text price;
+    @FXML
+    private Text description;
+    @FXML
+    private Circle downPlayerPhoto;
+    @FXML
+    private Text downPlayerNickname;
+    @FXML
+    private Text downPlayerLifePoint;
+    @FXML
+    private AnchorPane upperPlayerHand;
+    @FXML
+    private AnchorPane upperPlayerSpellZone;
+    @FXML
+    private AnchorPane upperPlayerMonsterZone;
+    @FXML
+    private AnchorPane phases;
+    @FXML
+    private AnchorPane downPlayerMonsterZone;
+    @FXML
+    private AnchorPane downPlayerSpellZone;
+    @FXML
+    private AnchorPane downPlayerHand;
+
+
     public Pane root;
     public AnchorPane addCardArea;
     private Game game;
+
     @FXML
-    public void initialize(){
+    public void initialize() {
         game = Duel.getCurrentDuel().getGame();
 //        initZones();
         initPhases();
@@ -37,8 +77,16 @@ public class GamePlay extends Menu{
 
 
     private void initDuelistInfo() {
-
-
+        upperPlayerNickname.setText(game.getBoard().getMainPlayer().getNickname());
+        upperPlayerLifePoint.setText(String.valueOf(game.getBoard().getMainPlayer().getLifePoint()));
+        upperPlayerPhoto.setStroke(Color.TRANSPARENT);
+//        Image image = game.getBoard().getMainPlayer().getProfileImage();
+//        upperPlayerPhoto.setFill(new ImagePattern(image));
+        downPlayerNickname.setText(game.getBoard().getRivalPlayer().getNickname());
+        downPlayerLifePoint.setText(String.valueOf(game.getBoard().getRivalPlayer().getLifePoint()));
+        downPlayerPhoto.setStroke(Color.TRANSPARENT);
+//        image = game.getBoard().getRivalPlayer().getProfileImage();
+//        downPlayerPhoto.setFill(new ImagePattern(image));
     }
 
     private void initZones() {
@@ -63,12 +111,15 @@ public class GamePlay extends Menu{
 //    }
 
     private void initHands() {
-        view.getChildren().add(new RivalHand(game));
-        view.getChildren().add(new Hand(game, view));
+        upperPlayerHand.getChildren().add(new RivalHand(game));
+        downPlayerHand.getChildren().add(new Hand(game, view));
     }
 
     private void initPhases() {
-        view.getChildren().add(new Phases(game));
+        phases.getChildren().add(new Phases(game));
     }
 
+    public void back(ActionEvent actionEvent) {
+        MainGraphic.setRoot("MainMenu");
+    }
 }
