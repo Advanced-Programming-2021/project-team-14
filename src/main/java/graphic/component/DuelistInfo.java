9c246cd61d0294c671848cd5eb981f7d3fbcbce1
sajad <s.soltaniant@gmail.com>
2021-07-07 21:33:56 +0330

    add duelist info

--- src/main/java/graphic/component/DuelistInfo.java
+++ src/main/java/graphic/component/DuelistInfo.java
@@ -1,5 +1,6 @@
 package graphic.component;
 
+import com.jfoenix.controls.JFXProgressBar;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -17,13 +18,14 @@ public class DuelistInfo extends AnchorPane implements ComponentLoader {
     private Text username, nickname;
     @FXML
     private ImageView profilePhoto;
+//    @FXML
+//    private JFXProgressBar lifePoint;
 
     public DuelistInfo(User user, boolean isOpponent) {
         load("DuelistInfo");
+//        lifePoint.setProgress(0.5);
         username.setText(user.getUsername());
         nickname.setText(user.getNickname());
-        this.setLayoutX(900);
-        this.setLayoutY(720);
         if (isOpponent) {
             this.setLayoutY(5);
         }
@@ -31,7 +33,8 @@ public class DuelistInfo extends AnchorPane implements ComponentLoader {
             Image image = Database.getProfilePhoto(user.getUsername());
             profilePhoto.setImage(image);
         } else {
-            profilePhoto.setStyle("-fx-background-color: BLUE;");
+            Image image = Database.getProfilePhoto("default");
+            profilePhoto.setImage(image);
         }
     }
 }
