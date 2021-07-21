4145522fd97a375c4a3d6b3ed6acac1a3217b56e
FatemehAlimoradi <f.alimoradi80@gmail.com>
2021-07-21 16:01:44 +0430

    tv

---
+++ src/main/java/model/SaveGameLogs.java
@@ -0,0 +1,48 @@
+package model;
+
+import com.google.gson.JsonObject;
+import model.game.Duel;
+import org.json.JSONObject;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class SaveGameLogs {
+
+    private static ArrayList<SaveGameLogs> allGames = new ArrayList<>();
+
+    private Duel duel;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private ArrayList<JSONObject> logs;
+
+    public SaveGameLogs(Duel duel, JSONObject log){
+        this.duel = duel;
+        this.startTime = LocalDateTime.now();
+        this.logs = new ArrayList<>();
+        this.logs.add(log);
+    }
+
+    public void addLog(JSONObject log){
+        logs.add(log);
+    }
+
+    public void finishGame(){
+        endTime = LocalDateTime.now();
+        TV.removeOnlineGame(this.getDuel());
+    }
+
+    public static void addGame(SaveGameLogs game) {
+        allGames.add(game);
+        TV.addOfflineGame(game.getDuel());
+    }
+
+    public Duel getDuel() {
+        return duel;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+}
